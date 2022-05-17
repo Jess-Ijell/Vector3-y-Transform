8df93790d2ca334201477d39a9c4d92242e6daf9
Jessi Ijelman Lipovetzky <46876160@est.ort.edu.ar>
2022-05-17 11:34:45 -0300

    Vector3 y Transform

---
+++ Assets/Vector3Script.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Vector3Script : MonoBehaviour
+{
+    public Vector3 myVector3;
+    public GameObject myObject;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        myObject.transform.position = myVector3;
+        //myObject.transform.localScale = myVector3;
+        //myObject.transform.eulerAngles = myVector3;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
